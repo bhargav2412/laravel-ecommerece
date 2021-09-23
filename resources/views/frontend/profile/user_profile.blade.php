efd009f9a90804114156762f3c3937b31eed2439
bhargav2412 <bhargavgohel10@gmail.com>
2021-09-23 19:43:01 +0530

    backend CRUD

--- resources/views/frontend/profile/user_profile.blade.php
+++ resources/views/frontend/profile/user_profile.blade.php
@@ -6,12 +6,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2"><br>
-                    <img src="{{ !empty($editAdminData->profile_photo_path) ? url('upload/admin_image/' . $editAdminData->profile_photo_path) : url('upload/no_image.jpg') }}"
-                        class="card-img-top" style="border-radius: 50%" height="100%" width="100%"><br><br>
+                    <img class="card-img-top" style="border-radius: 50%"
+                        src="{{ !empty($user->profile_photo_path) ? url('upload/user_images/' . $user->profile_photo_path) : url('upload/no_image.jpg') }}"
+                        height="100%" width="100%">
+                    <br><br>
                     <ul class="list-group list-group-flush">
                         <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
                         <a href="{{ route('user.profile') }}" class="btn btn-primary btn-sm btn-block">Profile Update</a>
-                        <a href="" class="btn btn-primary btn-sm btn-block">Change Password</a>
+                        <a href="{{ route('change.password') }}" class="btn btn-primary btn-sm btn-block">Change Password</a>
                         <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
                     </ul>
                 </div>
@@ -21,13 +23,13 @@
                 <div class="col-md-6">
                     <div class="card">
                         <h3 class="text-center"><span
-                                class="text-danger">Hii...</span><strong>{{ Auth::user()->name }} </strong>Welcome To
-                            Shopping Site</h3>
+                                class="text-danger">Hi....</span><strong>{{ Auth::user()->name }}</strong> Update Your
+                            Profile </h3>
+
 
 
                         <div class="card-body">
-                            <form method="POST" action="{{ route('admin.profile.update') }}"
-                                enctype="multipart/form-data">
+                            <form method="post" action="{{ route('user.profile.store') }}" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group">
                                     <label class="info-title">Name <span>*</span></label>
@@ -40,8 +42,12 @@
                                         value="{{ $user->email }}">
                                 </div>
                                 <div class="form-group">
-                                    <label class="info-title" for="exampleInputEmail1">Profile <span>*</span></label>
-                                    <input type="file" class="form-control unicase-form-control text-input" name="email">
+                                    <label class="info-title" for="exampleInputEmail1">User Image <span> </span></label>
+                                    <input type="file" name="profile_photo_path" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-danger">Update</button>
                                 </div>
                             </form>
                         </div>
